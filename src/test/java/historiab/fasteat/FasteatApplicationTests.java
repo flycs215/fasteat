bca441285bddb6ba0a8648eb98a8d1301895915f
Alejandro Chung <flycs215@gmail.com>
2020-12-30 18:20:38 -0800

    commit_message

---
+++ src/test/java/historiab/fasteat/FasteatApplicationTests.java
@@ -0,0 +1,13 @@
+package historiab.fasteat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasteatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
